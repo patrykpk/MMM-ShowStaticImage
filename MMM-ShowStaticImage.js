a83cd0e7cf27c1725432952374f02bbcebd89ecf
Patryk Krzyzaniak <54407312+patrykpk@users.noreply.github.com>
2020-07-16 17:20:14 +0200

    Added error message if an error occurs while loading a file

--- MMM-ShowStaticImage.js
+++ MMM-ShowStaticImage.js
@@ -5,20 +5,41 @@
  */
 
 Module.register("MMM-ShowStaticImage", {
-    defaults: {
-            image: "",
-            imageScale: "30%"
-    },
-
-    getDom: function(){
-            var wrapper = document.createElement("div");
-            var imageStatic = document.createElement("img");
-            let imagePath = "modules/MMM-ShowStaticImage/images/";
-            
-            imageStatic.setAttribute("src", imagePath + this.config.image);
-            imageStatic.setAttribute("width", this.config.imageScale);
-            wrapper.appendChild(imageStatic);
-            return wrapper;
-
-    },
+        defaults: {
+                image: "",
+                imageScale: "100%"
+        },
+        
+        getDom: function(){
+                var wrapper = document.createElement("div");
+                
+                var imageStatic = document.createElement("img");
+                let imagePath = "modules/MMM-ShowStaticImage/images/";
+                imageStatic.setAttribute("src", imagePath + this.config.image);
+                
+                imageStatic.onerror = () => {
+                        wrapper.className = "small dimmed";
+                        var errorMessage = this.errorOccured();
+                        wrapper.innerHTML = errorMessage;
+                };
+                
+                imageStatic.onload = () => {
+                        var scaleFactor = this.config.imageScale;
+                        var resizeImage = parseFloat(scaleFactor)/100*imageStatic.width;
+                        imageStatic.setAttribute("width", `${resizeImage}px`);
+                };        
+                
+                wrapper.appendChild(imageStatic);
+                return wrapper;
+        },
+        
+        errorOccured: function(){
+                if (this.config.image === ""){
+                        errorMsg = "Required paramter missing: No image path was given";
+                }
+                else{
+                        errorMsg = "An error occured. Could not load this image.";
+                }
+                return errorMsg;
+        }
 });
